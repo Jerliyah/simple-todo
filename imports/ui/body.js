be09226c6316cf8be0c8c555591d0b02c71c3dfd
Jerliyah <jerliyahcraig@gmail.com>
2017-12-06 16:41:31 -0600

    task submission

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -9,5 +9,24 @@ Template.body.helpers({
     tasks() { return Tasks.find({}) }
 });
 
+Template.body.events({
+    'submit .new-task'(event) {
 
+        event.preventDefault();
+
+        // get value from form element
+        const text = event.target.text.value;
+        console.log(event)
+
+        // Insert typed task into collection
+        Tasks.insert({
+            text,
+            createdAt: new Date()
+        });
+
+        // Clear form
+        event.target.text.value = '';
+
+    }
+})
 
